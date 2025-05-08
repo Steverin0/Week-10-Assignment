c6ca3083c1d2b3fb488cd3cd7eb9f550b0effa85
Steverin0 <scabrera28@gmail.com>
2025-05-08 14:11:30 -0600

    finished with the bare minimum and a little style. Might add more later in the week

--- script.js
+++ script.js
@@ -2,3 +2,42 @@
 // A Bootstrap styled table representing your choice of data.
 // A Bootstrap styled form that allows a user to add a new row to the table when clicking on submit.
 
+const formButton = document.getElementById("formButton")
+
+let number = 1
+
+formButton.addEventListener("click", (event) => {
+    event.preventDefault() //this stops the click from refreshing the page and deleting all the input data
+    
+    
+    let firstName = document.getElementById("firstInput").value
+    let lastName = document.getElementById("secondInput").value
+    let duty = document.getElementById("thirdInput").value
+
+    let newTableRow = document.createElement("tr")
+
+    let numberNode = document.createElement("td")
+    numberNode.innerHTML = number
+    newTableRow.append(numberNode)
+
+    let firstNameNode = document.createElement("td")
+    firstNameNode.innerHTML = firstName
+    newTableRow.append(firstNameNode)
+
+    let lastNameNode = document.createElement("td")
+    lastNameNode.innerHTML = lastName
+    newTableRow.append(lastNameNode)
+
+    let dutyNode = document.createElement("td")
+    dutyNode.innerHTML = duty
+    newTableRow.append(dutyNode)
+    //console.log(newTableRow) this showsthe user input into the console, so yay!
+
+    document.getElementById("tBody").appendChild(newTableRow)
+    //make sure this is in the function of the button so it actually works -_-
+    document.getElementById("firstInput")
+    document.getElementById("secondInput")
+    document.getElementById("thirdInput")
+
+    number++
+})
